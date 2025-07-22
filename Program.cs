2729fcb85f2bfab8dbd4d8f507c85b2ef19adc0a
Frank <Moehlenkamp.Frank@gmail.com>
2025-07-22 11:31:20 +0200

    Neue CW line (17)

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace GitTest
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hallo Herr Sprenger");
             Console.WriteLine("Hallo Herr Aui");
+            Console.WriteLine("Test 3");
         }
     }
 }
