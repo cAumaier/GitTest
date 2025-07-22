19e066ed99a37804fd5ea02dbb829bacc0462af0
Christian Aumaier <Christianaumaier@googlemail.com>
2025-07-22 10:06:54 +0200

    Ausgebessert

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace GitTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.Writeline("Hello World!")
+            Console.WriteLine("Hello World!");
         }
     }
 }
