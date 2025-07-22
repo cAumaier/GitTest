b57724cfef1f99385fb7947dd0bc8caf0dbbe5b0
Christian Aumaier <Christianaumaier@googlemail.com>
2025-07-22 10:49:24 +0200

    cw Sprenger

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace GitTest
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hallo Herr Sprenger");
         }
     }
 }
